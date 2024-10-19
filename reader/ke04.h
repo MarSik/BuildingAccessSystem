37f2c9187a0f58c5a0b160fca7a13044851c009d
Martin Sivak <mars@montik.net>
2024-10-19 10:28:32 +0200

    Missing files?

---
+++ reader/ke04.h
@@ -0,0 +1,14 @@
+#ifndef KE04_H_OK7MS
+#define KE04_H_OK7MS
+
+#include <stdint.h>
+
+#define IRQn_Type uint8_t
+#define SysTick_IRQn ((IRQn_Type)15)
+#define __NVIC_PRIO_BITS 2
+
+#include "core_cm0plus.h"
+
+
+
+#endif
---
+++ reader/main.cpp
@@ -0,0 +1,12 @@
+#include "ke04.h"
+
+extern "C" {
+void LowLevelInit(void) {
+}
+
+void SystemInit(void) {
+}
+
+int main(void) {
+}
+}
